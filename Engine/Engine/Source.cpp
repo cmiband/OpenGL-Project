67d700103ad339791e44c716a58d42f94fa91a85
cmiband <bartadamo2005@gmail.com>
2022-08-21 19:37:08 +0200

    Setting up
    glfw and glew

---
+++ Engine/Engine/Source.cpp
@@ -0,0 +1,36 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (GLEW_OK != glewInit()) {
+        std::cout << "error" << std::endl;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
