814e488a8ed1bb96a3a69af7a4f7659d13912ecc
cmiband <bartadamo2005@gmail.com>
2022-10-31 17:23:29 +0100

    finally learnt how to draw a circle

--- Engine/Engine/src/Application.cpp
+++ Engine/Engine/src/Application.cpp
@@ -67,11 +67,74 @@ void Application::Run() {
 
 	Renderer renderer;
 
+	int sides = 360;
+	float r = 0.5f;
+	float x= 0.0f;
+	float y = 0.0f;
+
+	int numberOfVertices = sides + 2;
+	float doublePi = 2.0f * M_PI;
+
+	float* circleVerticesX = new float[numberOfVertices];
+	float* circleVerticesY = new float[numberOfVertices];
+
+	circleVerticesX[0] = x;
+	circleVerticesY[0] = y;
+
+	for (int i = 1; i < numberOfVertices; i++) {
+		circleVerticesX[i] = x + (r * cos(i * doublePi / sides));
+		circleVerticesY[i] = y + (r * sin(i * doublePi / sides));
+	}
+
+	float* allCircleVertices = new float[numberOfVertices * 2];
+	for (int i = 0; i < numberOfVertices; i++) {
+		allCircleVertices[i * 2] = circleVerticesX[i];
+		allCircleVertices[(i * 2) + 1] = circleVerticesY[i];
+	}
+	for (int i = 0; i < numberOfVertices; i++) {
+		std::cout << allCircleVertices[i * 2] << " " << allCircleVertices[(i * 2) + 1] << std::endl;
+	}
+
+	VertexBuffer vb(numberOfVertices * 2 * sizeof(float), allCircleVertices);
+
+	VertexArray va;
+	va.AddBuffer(2, false, 2 * sizeof(float));
+
+	unsigned int *indices = new unsigned int[3 * numberOfVertices];
+	int c = 0;
+	int i = 0;
+	while (i<numberOfVertices*3) {
+		if (i % 3 == 0) {
+			indices[i] = 0;
+			c++;
+		}
+		else if (i % 3 == 1) {
+			indices[i] = c;
+			c++;
+		}
+		else {
+			indices[i] = c;
+			c--;
+		}
+		i++;
+	}
+
+	IndexBuffer ib(numberOfVertices * 3 * sizeof(unsigned int), indices);
+
+	Shader shader("res/shaders/testShader.shader");
+
+	shader.Unbind();
+	vb.Unbind();
+	va.Unbind();
+	ib.Unbind();
 	while (!glfwWindowShouldClose(window))
 	{
 		renderer.Clear();
 
-		DrawCircle(0, 0, 0.5f, 360);
+		shader.Bind();
+		ib.Bind();
+		va.Bind();
+		GLCall(glDrawElements(GL_TRIANGLE_FAN, ib.getSize(), GL_UNSIGNED_INT, nullptr));
 
 		renderer.Swap(window);
 	}
