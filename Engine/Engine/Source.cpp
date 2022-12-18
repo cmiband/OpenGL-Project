3e8d3dec726ff54d86dfc63fca34652af2e7ee66
cmiband <bartadamo2005@gmail.com>
2022-12-18 12:15:41 +0100

    setting up

--- Engine/Engine/Source.cpp
+++ Engine/Engine/Source.cpp
@@ -7,12 +7,62 @@ int main()
     Window window(SCREEN_WIDTH, SCREEN_HEIGHT, "Mikoshi");
     Renderer r;
 
-    Circle circle(glm::vec2{ 100.0f, 100.0f }, 50.0f, math::Color4<float>{0.5f, 0.5f, 0.8f, 1.0f});
+    float ballWidth = 10.0f;
+    float racketWidth = 80.0f;
+    Rectangle ball(glm::vec2{ 80.0f, 10.0f }, ballWidth, ballWidth, math::Color4<float>{0.1f, 0.1f, 0.9f, 1.0f});
+    Rectangle racket(glm::vec2{ SCREEN_WIDTH / 2, 30.0f }, racketWidth, 10.0f, math::Color4<float>{0.5f, 0.9f, 0.1f, 1.0f});
+
+    float ballSpeedX = 5.0f;
+    float ballSpeedY = 5.0f;
+    float racketSpeed = 5.0f;
+    bool moveUp = true;
+    bool moveRight = true;
 
     while (!window.shouldClose()) {
-        r.Clear(); 
+        r.Clear();
 
-        circle.Draw(r);
+        if (ball.GetPosition().y >= SCREEN_HEIGHT - ballWidth) {
+            moveUp = false;
+        }
+        if (ball.GetPosition().y <= 0) {
+            moveUp = true;
+        }
+        if (ball.GetPosition().x >= SCREEN_WIDTH - ballWidth) {
+            moveRight = false;
+        }
+        if (ball.GetPosition().x <= 0) {
+            moveRight = true;
+        }
+
+        if (racket.CollidesWith(ball)) {
+            moveUp = true;
+            std::cout << "Collision" << std::endl;
+        }
+
+        if (moveRight) {
+            ballSpeedX = 5.0f;
+        }
+        else {
+            ballSpeedX = -5.0f;
+        }
+        if (moveUp) {
+            ballSpeedY = 5.0f;
+        }
+        else {
+            ballSpeedY = -5.0f;
+        }
+
+        if (Keyboard::KeyPressed(window, GLFW_KEY_A) && racket.GetPosition().x >= 0) {
+            racket.Move(glm::vec2{ -racketSpeed,0 });
+        }
+        if (Keyboard::KeyPressed(window, GLFW_KEY_D) && racket.GetPosition().x <= SCREEN_WIDTH-racketWidth) {
+            racket.Move(glm::vec2{ racketSpeed,0 });
+        }
+
+        ball.Move(glm::vec2{ ballSpeedX,ballSpeedY });
+
+        ball.Draw(r);
+        racket.Draw(r);
 
         r.Swap(window);
     }
